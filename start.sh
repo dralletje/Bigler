f9a174f1bf29d53d52492072ecfc78363961a4aa
Plancke <Plancke@users.noreply.github.com>
2014-10-05 23:04:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@
 JAR_NAME=spigot.jar
 PERM_SIZE=128M
 RAM=1024MB
-RAM_2=1024MB
 
-java -Xmx$RAM -Xms$RAM_2 -XX:MaxPermSize=$PERM_SIZE -jar $JAR_NAME
+java -Xmx$RAM -Xms$RAM -XX:MaxPermSize=$PERM_SIZE -jar $JAR_NAME
